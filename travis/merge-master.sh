1ce0f0452a46588904f9d25c6f6b21575552221f
Sidney N'Guessan <snguessan@dxo.com>
2015-05-06 15:30:13 +0200

    [travis] verbosing

--- travis/merge-master.sh
+++ travis/merge-master.sh
@@ -6,9 +6,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
     exit 0
 fi
 
-set -x
-
-git checkout master  	   || exit 255
-git merge "$TRAVIS_BRANCH" || exit 254
-git push origin master     || exit 253
+git fetch origin master:master || exit 1
+git checkout master  	       || exit 255
+git merge "$TRAVIS_BRANCH"     || exit 254
+git push origin master         || exit 253
 
