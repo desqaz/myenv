4156f66dd7bbd542f6b193391e017105c0389a6a
hitite <hitite@gmail.com>
2015-05-19 17:21:38 +0200

    [travis] update automerge script to master : add commit message

--- travis/merge-master.sh
+++ travis/merge-master.sh
@@ -11,12 +11,11 @@ git config user.name  "travis-ci"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-git fetch origin master:master 							|| exit 1
-git checkout master  	       							|| exit 2
-git merge "$TRAVIS_BRANCH"     							|| exit 3
+git fetch origin master:master || exit 1
+git checkout master || exit 2
+git merge "$TRAVIS_BRANCH" -m "[travis] Automerge" --log || exit 3
 
-git remote add\
-  	origin_http https://github.com/desqaz/myenv.git || exit 4
+git remote add origin_http https://github.com/desqaz/myenv.git || exit 4
 
-git push origin_http master    							|| exit 5
+git push origin_http master || exit 5
 
