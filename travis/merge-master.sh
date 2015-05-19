ca5d8ddc435efdcdc8aac5e02cf29eba988ec51c
hitite <hitite@gmail.com>
2015-05-19 17:13:09 +0200

    [travis] update automerge script to master

--- travis/merge-master.sh
+++ travis/merge-master.sh
@@ -6,6 +6,11 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
     exit 0
 fi
 
+git config user.email "travis@travis-ci.com"
+git config user.name  "travis-ci"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
 git fetch origin master:master 							|| exit 1
 git checkout master  	       							|| exit 2
 git merge "$TRAVIS_BRANCH"     							|| exit 3
