71c2a70846215e57c491e86743f7c71277bd486a
Sidney N'Guessan <snguessan@dxo.com>
2015-05-22 15:09:07 +0200

    [setup] Add mechanism to install deps

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ myenvcusto=$myenv/custom/user/$USER
 cd $HOME
 
 # link rc files
+echo "Linking rc files"
+
 for rc in $(find $myenv -follow -maxdepth 1  -type f  -name ".*rc"); do
 	ln -svf $rc
 done
@@ -28,6 +30,42 @@ ln -svf $myenv/git/.gitk
 # needed user tree
 [ ! -d $HOME/tmp ]    && mkdir -v $HOME/tmp
 [ ! -d $HOME/.local ] && mkdir -v $HOME/.local
+
+
+#
+# Packages installation
+#
+echo "[0;32mInstalling deps packages[0m"
+type apt-get 2&>1 > /dev/null
+
+if [ $? -eq 0 ]; then
+	for dep in $(cat $myenv/tools/deps.lst | grep '^pkg' | awk '{print $2}'); do
+		sudo apt-get install "$dep"
+	done
+fi
+
+#
+# Binaries installation 
+#
+echo "[0;32mInstalling deps binaries[0m"
+type yum 2&>1 > /dev/null
+
+if [ $? -eq 0 ]; then
+	for dep in $(cat $myenv/tools/deps.lst | grep '^pip' | awk '{print $2}'); do
+		sudo pip install $dep
+	done
+fi
+
+#
+# Others installation
+#
+echo "[0;32mInstalling others deps[0m"
+
+for dep in "$(cat $myenv/tools/deps.lst | grep '^cmd' |  awk '{for(i=1;i<$$NF;i++) $i=""; print}')"; do
+	echo "[0;33m$dep[0m"
+	sh -c "$dep"
+done
+
 
 # enjoy!
 
